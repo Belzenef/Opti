8db30e0324affd0db3935606adbc3a9fb72b2b21
Belzenef <33092663+Belzenef@users.noreply.github.com>
2018-11-15 11:50:24 +0100

    Add files via upload

---
+++ statsF.R
@@ -0,0 +1,59 @@
+rm(list = ls())
+
+library(ggplot2)
+library(doBy)
+
+data1<-read.table("fonctionf/paramsf.txt", h=T, dec=".")
+
+# 1D
+Equilibre<-as.factor((data1$Solution>(-2.9) & data1$Solution<(-2.7))+(data1$Solution >(3.4) & data1$Solution<(3.6))*2)
+data1<-cbind(data1,Equilibre)
+data1$Equilibre<-as.factor(data1$Equilibre)
+data1$k1<-as.factor(data1$k1)
+data1$k2<-as.factor(data1$k2)
+data1$Depart<-as.factor(data1$Depart)
+#data1$nbIter[data1$nbIter>=50000]<-25000
+Overt<-as.factor(data1$nbIter>=10000)
+data1<-cbind(data1,Overt)
+summary(data1)
+
+p <- ggplot(data1, aes(x = Depart, y =nbIter, color = Equilibre)) + 
+  geom_jitter(size=2) + 
+  ggtitle(" ") + theme_bw() +
+  facet_wrap(~ k1+k2)+ 
+  theme(axis.title=element_text(size=11,face="bold"))
+p 
+#ggsave("evolequif.png",p)
+test<-data1[801:1000,]
+summary(test)
+summary(test[test$Depart==2,])
+summaryBy(nbIter~k1+k2+Depart, data=data1, FUN=c(mean))
+
+#**********
+data1<-read.table("fonctiong/paramsg4.txt", h=T, dec=".")
+# 2D
+Equilibre<-as.factor(
+  ((data1$X >(-2.9) & data1$X<(-2.7)) & (data1$Y<(-2.7) & data1$Y>(-2.9)))*1+
+  ((data1$X >(3.4) & data1$X<(3.6)) & (data1$Y<(-2.7) & data1$Y>(-2.9)))*2+
+  ((data1$X<(-2.7) & data1$X>(-2.9)) & (data1$Y<(3.6) & data1$Y>(3.4)))*3+
+  ((data1$X<(3.6) & data1$X>(3.4)) & (data1$Y<(3.6) & data1$Y>(3.4)))*4)
+data1<-cbind(data1,Equilibre)
+data1$Equilibre<-as.factor(data1$Equilibre)
+data1$k1<-as.factor(data1$k1)
+data1$k2<-as.factor(data1$k2)
+data1$Depart<-as.factor(data1$Depart)
+#data1$nbIter[data1$nbIter>=100000]<-10**(4.5)
+Overt<-as.factor(data1$nbIter>=20000)
+data1<-cbind(data1,Overt)
+summary(data1)
+p <- ggplot(data1, aes(x = Depart, y =nbIter, color = Equilibre)) + 
+  geom_jitter(size=2) + 
+  ggtitle(" ") + theme_bw() +
+  facet_wrap(~ k1+k2)+ 
+  theme(axis.title=element_text(size=11,face="bold"))
+p
+test<-data1[data1$k1==10 & data1$k2==0.1,]
+summary(test)
+summary(test[test$Depart==-4,c("k1","k2","Depart","nbIter","Equilibre","Overt")])
+summary(test[test$Depart==4,c("k1","k2","Depart","nbIter","Equilibre","Overt")])
+#ggsave("evolequig4.png",p)
